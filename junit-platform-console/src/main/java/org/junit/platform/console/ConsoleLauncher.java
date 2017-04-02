8638dfc4135fecb2954a3323c937b148f979d27f
Christian Stein <sormuras@gmail.com>
2017-04-02 20:20:00 +0200

    Introduce static ConsoleLauncher.launch entry point
    Before this commit the only static main(String[]) entry point called
    System.exit(int) to report its exit code to the parent process. That
    potentially broke external tool work flows, as the entire VM was killed.
    
    Now, external tool may invoke the static launch(...) method to get
    the arguments parsed, the tests executed and a result object holding
    execution information for own examination.
    
    Fixes #763

--- junit-platform-console/src/main/java/org/junit/platform/console/ConsoleLauncher.java
+++ junit-platform-console/src/main/java/org/junit/platform/console/ConsoleLauncher.java
@@ -10,6 +10,7 @@
 
 package org.junit.platform.console;
 
+import static org.junit.platform.commons.meta.API.Usage.Experimental;
 import static org.junit.platform.commons.meta.API.Usage.Maintained;
 
 import java.io.BufferedWriter;
@@ -36,10 +37,15 @@ public class ConsoleLauncher {
 
 	@API(Maintained)
 	public static void main(String... args) {
-		CommandLineOptionsParser parser = new JOptSimpleCommandLineOptionsParser();
-		ConsoleLauncher consoleLauncher = new ConsoleLauncher(parser, System.out, System.err);
-		int exitCode = consoleLauncher.execute(args).getExitCode();
+		int exitCode = launch(System.out, System.err, args).getExitCode();
 		System.exit(exitCode);
+	}
+
+	@API(Experimental)
+	public static ConsoleLauncherExecutionResult launch(PrintStream out, PrintStream err, String... args) {
+		CommandLineOptionsParser parser = new JOptSimpleCommandLineOptionsParser();
+		ConsoleLauncher consoleLauncher = new ConsoleLauncher(parser, out, err);
+		return consoleLauncher.execute(args);
 	}
 
 	private final CommandLineOptionsParser commandLineOptionsParser;
