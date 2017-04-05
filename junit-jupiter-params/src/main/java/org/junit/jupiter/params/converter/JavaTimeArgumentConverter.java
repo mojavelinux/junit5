7b749092044fbbf5a91679a6d87605395f7a7b76
Christian Stein <sormuras@gmail.com>
2017-04-05 14:18:35 +0200

    Use reflection

--- junit-jupiter-params/src/main/java/org/junit/jupiter/params/converter/JavaTimeArgumentConverter.java
+++ junit-jupiter-params/src/main/java/org/junit/jupiter/params/converter/JavaTimeArgumentConverter.java
@@ -10,11 +10,8 @@
 
 package org.junit.jupiter.params.converter;
 
-import java.lang.invoke.LambdaMetafactory;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.time.temporal.TemporalQuery;
 
 import org.junit.jupiter.params.support.AnnotationConsumer;
@@ -35,21 +32,15 @@ class JavaTimeArgumentConverter extends SimpleArgumentConverter
 	@Override
 	public Object convert(Object input, Class<?> targetClass) throws ArgumentConversionException {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-		TemporalQuery<?> temporalQuery = createTemporalQuery(targetClass);
+		TemporalQuery<?> temporalQuery = (TemporalQuery<Object>) temporal -> {
+			try {
+				return targetClass.getMethod("from", TemporalAccessor.class).invoke(null, input);
+			}
+			catch (Throwable t) {
+				throw new AssertionError(t);
+			}
+		};
 		return formatter.parse(input.toString(), temporalQuery);
-	}
-
-	static TemporalQuery<?> createTemporalQuery(Class<?> targetClass) {
-		try {
-			MethodType methodType = MethodType.methodType(TemporalQuery.class);
-			MethodHandles.Lookup lookup = MethodHandles.lookup();
-			MethodHandle handle = lookup.findStatic(targetClass, "from", methodType);
-			return (TemporalQuery<?>) LambdaMetafactory.metafactory(lookup, "queryFrom", methodType,
-				methodType.generic(), handle, methodType).getTarget().invokeExact();
-		}
-		catch (Throwable t) {
-			throw new AssertionError(t);
-		}
 	}
 
 }
